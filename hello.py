8c4eef1a8912497ef84636ef74eebf90732103a5
Raj Koju <rajkoju@Mac.fios-router.home>
2025-08-05 16:45:19 -0400

    Init

---
+++ hello.py
@@ -0,0 +1,6 @@
+def get_word():
+    return "complete word"
+
+if __name__ == "__main__":
+    print(get_word())
+
---
+++ test_hello.py
@@ -0,0 +1,5 @@
+from hello import get_word 
+
+def test_get_word():
+    assert get_word() == "complete word"
+
